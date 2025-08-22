67c1da1049f7704ae089eb488c25435c495a07c1
同温层 <1998neutrino@gmail.com>
2025-08-22 20:28:14 +0800

    Reset root dir.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="artifact-flow",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),  # 指定在 src 目录下查找包
+    package_dir={"": "src"},              # 告诉 setuptools 包的根目录是 src
     python_requires=">=3.10",
 )
