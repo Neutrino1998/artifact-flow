ac0cc9b49d78da10a27b997ada635846c30b4fe6
同温层 <1998neutrino@gmail.com>
2025-09-16 09:59:36 +0800

    feat: update according to core progress

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,1 +1,52 @@
  
+"""
+Core模块
+提供LangGraph工作流和执行控制
+"""
+
+from core.state import (
+    AgentState,
+    create_initial_state,
+    update_state_for_routing,
+    update_state_for_confirmation,
+    extract_routing_from_response,
+    extract_confirmation_from_response
+)
+
+from core.graph import (
+    create_simple_graph,
+    create_graph_with_confirmation,
+    create_default_graph,
+    lead_agent_node,
+    search_agent_node,
+    crawl_agent_node,
+    user_confirmation_node,
+    route_after_lead,
+    route_after_subagent
+)
+
+from core.controller import ExecutionController
+
+__all__ = [
+    # State
+    "AgentState",
+    "create_initial_state",
+    "update_state_for_routing",
+    "update_state_for_confirmation",
+    "extract_routing_from_response",
+    "extract_confirmation_from_response",
+    
+    # Graph
+    "create_simple_graph",
+    "create_graph_with_confirmation",
+    "create_default_graph",
+    "lead_agent_node",
+    "search_agent_node",
+    "crawl_agent_node",
+    "user_confirmation_node",
+    "route_after_lead",
+    "route_after_subagent",
+    
+    # Controller
+    "ExecutionController"
+]
