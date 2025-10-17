99f95cd00e57d49ea25c9609bc956fe2cfe6ff91
同温层 <1998neutrino@gmail.com>
2025-10-17 11:23:30 +0800

    feat: change python requirement from 3.10 to 3.11 due to langgraph supporting issue

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),  # 指定在 src 目录下查找包
     package_dir={"": "src"},              # 告诉 setuptools 包的根目录是 src
-    python_requires=">=3.10",
+    python_requires=">=3.11",
 )
