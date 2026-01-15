9f8ef4af1ece4fc824c91d37a947cf64e36f543c
Neutrino1998 <1998neutrino@gmail.com>
2026-01-15 15:55:39 +0800

    refactor: update manager usage for SQLite persistence

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -5,13 +5,13 @@ Core模块
 
 from core.graph import (
     ExtendableGraph,
-    create_multi_agent_graph
+    create_multi_agent_graph,
+    create_async_sqlite_checkpointer
 )
 
-from core.controller import (
-    ExecutionController,
-    ConversationManager
-)
+from core.controller import ExecutionController
+
+from core.conversation_manager import ConversationManager
 
 from core.state import (
     AgentState,
@@ -27,17 +27,18 @@ __all__ = [
     # Graph
     "ExtendableGraph",
     "create_multi_agent_graph",
-    
+    "create_async_sqlite_checkpointer",
+
     # Controller
     "ExecutionController",
     "ConversationManager",
-    
+
     # State
     "AgentState",
     "ExecutionPhase",
     "create_initial_state",
     "merge_agent_response_to_state",
-    
+
     # Context
     "ContextManager",
 ]
